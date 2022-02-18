1559bc4ae71ec67cf8c5e5f777ac29e55d29da5e
Bruno Minelli <minellibruno@hotmail.com>
2022-02-18 16:50:52 -0300

    Requisito 09 concluido.

---
+++ src/components/MissionCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MissionCard extends React.Component {
+  render() {
+    const { name, year, country, destination } = this.props;
+    return (
+      <div data-testid="mission-card">
+        <p data-testid="mission-name">{`Missão: ${name}`}</p>
+        <p data-testid="mission-year">{`Ano: ${year}`}</p>
+        <p data-testid="mission-country">{`País: ${country}`}</p>
+        <p data-testid="mission-destination">{`Destino: ${destination}`}</p>
+      </div>
+    );
+  }
+}
+
+MissionCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  destination: PropTypes.string.isRequired,
+};
