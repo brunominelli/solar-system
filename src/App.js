01d80790f9579fa560407efdd873bf817a87a251
Bruno Minelli <minellibruno@hotmail.com>
2022-02-18 15:45:31 -0300

    Requisito 04 concluido.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 
 class App extends React.Component {
   render() {
-    const headline = 'Titulo';
     return (
       <>
         <Header />
         <SolarSystem />
-        <Title headline={ headline } />
       </>
     );
   }
